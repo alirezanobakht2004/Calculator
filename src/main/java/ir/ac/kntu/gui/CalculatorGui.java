57816085638654a40b8de149113fd3804669a146
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2023-05-25 22:55:25 +0330

    bugs fixed

--- src/main/java/ir/ac/kntu/gui/CalculatorGui.java
+++ src/main/java/ir/ac/kntu/gui/CalculatorGui.java
@@ -69,33 +69,43 @@ public class CalculatorGui {
     public void setEventsHandlers() {
         keyboardBan();
         zero.setOnMouseClicked(e -> {
+            afterEqual(textField.getText());
             textField.setText(textField.getText() + "0");
         });
         one.setOnMouseClicked(e -> {
+            afterEqual(textField.getText());
             textField.setText(textField.getText() + "1");
         });
         two.setOnMouseClicked(e -> {
+            afterEqual(textField.getText());
             textField.setText(textField.getText() + "2");
         });
         three.setOnMouseClicked(e -> {
+            afterEqual(textField.getText());
             textField.setText(textField.getText() + "3");
         });
         four.setOnMouseClicked(e -> {
+            afterEqual(textField.getText());
             textField.setText(textField.getText() + "4");
         });
         five.setOnMouseClicked(e -> {
+            afterEqual(textField.getText());
             textField.setText(textField.getText() + "5");
         });
         six.setOnMouseClicked(e -> {
+            afterEqual(textField.getText());
             textField.setText(textField.getText() + "6");
         });
         seven.setOnMouseClicked(e -> {
+            afterEqual(textField.getText());
             textField.setText(textField.getText() + "7");
         });
         eight.setOnMouseClicked(e -> {
+            afterEqual(textField.getText());
             textField.setText(textField.getText() + "8");
         });
         nine.setOnMouseClicked(e -> {
+            afterEqual(textField.getText());
             textField.setText(textField.getText() + "9");
         });
         minus.setOnMouseClicked(e -> {
@@ -123,11 +133,24 @@ public class CalculatorGui {
         });
         equal.setOnMouseClicked(e -> {
             textField.setText(mathEqual(textField.getText()));
+
         });
     }
 
     public boolean operandContainer(String str) {
-        if (str.contains("/") || str.contains("+") || str.contains("-") || str.contains("*")) {
+        if (str.contains("/") || str.contains("+") || str.contains("*")) {
+            return true;
+        }
+        if (str.contains("-")) {
+            int count = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '-') {
+                    count++;
+                }
+            }
+            if (count == 1 && str.charAt(0) == '-') {
+                return false;
+            }
             return true;
         }
         return false;
@@ -137,6 +160,9 @@ public class CalculatorGui {
         if (operandContainer(str) && Character.isDigit(str.charAt(str.length() - 1))) {
             for (int i = 0; i < str.length(); i++) {
                 if (!Character.isDigit(str.charAt(i)) && str.charAt(i) != '.') {
+                    if (str.charAt(i) == '-' && i == 0) {
+                        continue;
+                    }
                     StringBuilder firstNum = new StringBuilder(str.substring(0, i));
                     StringBuilder secondNum = new StringBuilder(str.substring(i + 1));
                     StringBuilder operator = new StringBuilder(operatorSwitch(str.charAt(i)));
@@ -146,6 +172,12 @@ public class CalculatorGui {
             return str;
         }
         return str;
+    }
+
+    public void afterEqual(String str) {
+        if (str.contains(".") && !operandContainer(str)) {
+            textField.setText("");
+        }
     }
 
     public String operatorSwitch(char opr) {
@@ -179,97 +211,97 @@ public class CalculatorGui {
         colFive.setSpacing(10);
         textField.setPrefSize(500, 100);
         zero.setPrefSize(100, 100);
-        ImageView view0 = new ImageView(new Image("buttons/0.jpg"));
+        ImageView view0 = new ImageView(new Image("0.jpg"));
         view0.setPreserveRatio(true);
         view0.setFitHeight(100);
         zero.setGraphic(view0);
         zero.setPadding(Insets.EMPTY);
         one.setPrefSize(100, 100);
-        ImageView view1 = new ImageView(new Image("buttons/1.jpg"));
+        ImageView view1 = new ImageView(new Image("1.jpg"));
         view1.setPreserveRatio(true);
         view1.setFitHeight(100);
         one.setPadding(Insets.EMPTY);
         one.setGraphic(view1);
         two.setPrefSize(100, 100);
-        ImageView view2 = new ImageView(new Image("buttons/2.jpg"));
+        ImageView view2 = new ImageView(new Image("2.jpg"));
         view2.setPreserveRatio(true);
         view2.setFitHeight(100);
         two.setPadding(Insets.EMPTY);
         two.setGraphic(view2);
         three.setPrefSize(100, 100);
-        ImageView view3 = new ImageView(new Image("buttons/3.jpg"));
+        ImageView view3 = new ImageView(new Image("3.jpg"));
         view3.setPreserveRatio(true);
         three.setGraphic(view3);
         view3.setFitHeight(100);
         three.setPadding(Insets.EMPTY);
         four.setPrefSize(100, 100);
-        ImageView view4 = new ImageView(new Image("buttons/4.jpg"));
+        ImageView view4 = new ImageView(new Image("4.jpg"));
         view4.setPreserveRatio(true);
         view4.setFitHeight(100);
         four.setPadding(Insets.EMPTY);
         four.setGraphic(view4);
         five.setPrefSize(100, 100);
-        ImageView view5 = new ImageView(new Image("buttons/5.jpg"));
+        ImageView view5 = new ImageView(new Image("5.jpg"));
         view5.setPreserveRatio(true);
         view5.setFitHeight(100);
         five.setPadding(Insets.EMPTY);
         five.setGraphic(view5);
         six.setPrefSize(100, 100);
-        ImageView view6 = new ImageView(new Image("buttons/6.jpg"));
+        ImageView view6 = new ImageView(new Image("6.jpg"));
         view6.setPreserveRatio(true);
         view6.setFitHeight(100);
         six.setPadding(Insets.EMPTY);
         six.setGraphic(view6);
         seven.setPrefSize(100, 100);
-        ImageView view7 = new ImageView(new Image("buttons/7.jpg"));
+        ImageView view7 = new ImageView(new Image("7.jpg"));
         view7.setPreserveRatio(true);
         view7.setFitHeight(100);
         seven.setPadding(Insets.EMPTY);
         seven.setGraphic(view7);
         eight.setPrefSize(100, 100);
-        ImageView view8 = new ImageView(new Image("buttons/8.jpg"));
+        ImageView view8 = new ImageView(new Image("8.jpg"));
         view8.setPreserveRatio(true);
         view8.setFitHeight(100);
         eight.setPadding(Insets.EMPTY);
         eight.setGraphic(view8);
         nine.setPrefSize(100, 100);
-        ImageView view9 = new ImageView(new Image("buttons/9.jpg"));
+        ImageView view9 = new ImageView(new Image("9.jpg"));
         view9.setPreserveRatio(true);
         view9.setFitHeight(100);
         nine.setPadding(Insets.EMPTY);
         nine.setGraphic(view9);
         equal.setPrefSize(100, 100);
-        ImageView view10 = new ImageView(new Image("buttons/equal.jpg"));
+        ImageView view10 = new ImageView(new Image("equal.jpg"));
         view10.setPreserveRatio(true);
         view10.setFitHeight(100);
         equal.setPadding(Insets.EMPTY);
         equal.setGraphic(view10);
         minus.setPrefSize(100, 100);
-        ImageView view11 = new ImageView(new Image("buttons/minus.jpg"));
+        ImageView view11 = new ImageView(new Image("minus.jpg"));
         view11.setPreserveRatio(true);
         view11.setFitHeight(100);
         minus.setPadding(Insets.EMPTY);
         minus.setGraphic(view11);
         multiplication.setPrefSize(100, 100);
-        ImageView view12 = new ImageView(new Image("buttons/multiply.jpg"));
+        ImageView view12 = new ImageView(new Image("multiply.jpg"));
         view12.setPreserveRatio(true);
         view12.setFitHeight(100);
         multiplication.setPadding(Insets.EMPTY);
         multiplication.setGraphic(view12);
         clear.setPrefSize(100, 100);
-        ImageView view13 = new ImageView(new Image("buttons/clear.jpg"));
+        ImageView view13 = new ImageView(new Image("clear.jpg"));
         view13.setPreserveRatio(true);
         view13.setFitHeight(100);
         clear.setPadding(Insets.EMPTY);
         clear.setGraphic(view13);
         plus.setPrefSize(100, 100);
-        ImageView view14 = new ImageView(new Image("buttons/plus.jpg"));
+        ImageView view14 = new ImageView(new Image("plus.jpg"));
         view14.setPreserveRatio(true);
         view14.setFitHeight(100);
         plus.setPadding(Insets.EMPTY);
         plus.setGraphic(view14);
         division.setPrefSize(100, 100);
-        ImageView view15 = new ImageView(new Image("buttons/divide.jpg"));
+        ImageView view15 = new ImageView(new Image("divide.jpg"));
         view15.setPreserveRatio(true);
         view15.setFitHeight(100);
         division.setPadding(Insets.EMPTY);
